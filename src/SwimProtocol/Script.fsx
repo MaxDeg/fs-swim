f380584f60b3bc1d6e002e0e2223a864c5aa9b1e
MaxDeg <max.degallaix@outlook.com>
2016-06-15 15:19:49 +0200

    cleaning in code  - must add new library for MsgPack

--- src/SwimProtocol/Script.fsx
+++ src/SwimProtocol/Script.fsx
@@ -1,26 +1,18 @@
-// #r "../../packages/FSharpx.Collections/lib/net40/FSharpx.Collections.dll"
-// #r "../../packages/FSharp.Control.AsyncSeq/lib/net45/FSharp.Control.AsyncSeq.dll"
-// #r "../../packages/FSharpx.Async/lib/net40/FSharpx.Async.dll"
-// #r "../../packages/FSharpx.Extras/lib/40/FSharpx.Extras.dll"
-// #load "Utils.fs"
-// #load "Types.fs"
-// #load "Message.fs"
-// #load "Transport.fs"
-// #load "EventsDissemination.fs"
-// #load "Membership.fs"
-// #load "FailureDetection.fs"
-// #load "Swim.fs"
-#load "MsgPack.fs"
+ #r "../../packages/FSharpx.Collections/lib/net40/FSharpx.Collections.dll"
+ #r "../../packages/FSharp.Control.AsyncSeq/lib/net45/FSharp.Control.AsyncSeq.dll"
+ #r "../../packages/FSharpx.Async/lib/net40/FSharpx.Async.dll"
+ #r "../../packages/FSharpx.Extras/lib/40/FSharpx.Extras.dll"
+ #load "Utils.fs"
+ #load "Types.fs"
+ #load "Message.fs"
+ #load "Transport.fs"
+ #load "EventsDissemination.fs"
+ #load "Membership.fs"
+ #load "FailureDetection.fs"
+ #load "Swim.fs"
 
-// open SwimProtocol
-open MsgPack
-open MsgPack.Packer
 
-// let node1 = Swim.init { Swim.defaultConfig with Port = 1337 } []
-//let node2 = Swim.init { Swim.defaultConfig with Port = 1338 } [ ("mdg-ubuntu", 1337) ]
+open SwimProtocol
 
-let values = pack [| Nil; Bool true; UInt32 254u |]
-let encode = System.Convert.ToBase64String
-
-encode values
-
+let node1 = Swim.init { Swim.defaultConfig with Port = 1337 } []
+let node2 = Swim.init { Swim.defaultConfig with Port = 1338 } [ ("mdg-ubuntu", 1337) ]
