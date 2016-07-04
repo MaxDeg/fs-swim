90a2795885003f99df417ff1c832b50adfc40559
MaxDeg <max.degallaix@outlook.com>
2016-07-04 13:37:54 +0200

    some test

--- src/SwimProtocol/Script.fsx
+++ src/SwimProtocol/Script.fsx
@@ -37,7 +37,26 @@ let node3 =
                                         PeriodTimeout = TimeSpan.FromSeconds(1.) } [ (localName, 1337us) ]
 
 // -------------------------------------------------------------
+
 type Agent<'a> = MailboxProcessor<'a>
+
+type SeqNumber =
+    private | SeqNumber of uint64
+
+[<RequireQualifiedAccess>]
+module Sequence =
+    let make() = SeqNumber(0UL)
+    let incr = function
+        | SeqNumber s -> SeqNumber(s + 1UL)
+
+type IncarnationNumber =
+    private | IncarnationNumber of uint64
+
+[<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module IncarnationNumber =
+    let make() = IncarnationNumber(0UL)
+    let incr = function
+        | IncarnationNumber i -> IncarnationNumber(i + 1UL)
 
 [<StructuralEquality; StructuralComparison>]
 type Node =
@@ -45,17 +64,12 @@ type Node =
       Port : uint16 }
     override x.ToString() = string x.Port
 
-type IncarnationNumber =
-    | IncarnationNumber of uint64
-
-type SeqNumber =
-    | SeqNumber of uint64
-
 type NodeStatus =
     | Alive of IncarnationNumber
     | Suspect of IncarnationNumber
     | Dead of IncarnationNumber
 
+[<StructuralEquality; StructuralComparison>]
 type SwimEvent =
     | Membership of Node * NodeStatus
     | User of string
@@ -64,47 +78,110 @@ type SwimMessage =
     | Ping of SeqNumber
     | PingRequest of SeqNumber * Node
     | Ack of SeqNumber * Node
+    | Leave
 
-[<RequireQualifiedAccess>]
-module Sequence =
-    let make() = SeqNumber(0UL)
-    let incr = function
-        | SeqNumber s -> SeqNumber(s + 1UL)
+type Udp =
+    private { Client : System.Net.Sockets.UdpClient
+              ReceivedEvent : Event<Node * byte[]> }
 
 [<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module IncarnationNumber =
-    let make() = IncarnationNumber(0UL)
-    let incr = function
-        | IncarnationNumber i -> IncarnationNumber(i + 1UL)
+module Udp =
+    open System.Net
+    open System.Net.Sockets
+    open System.Net.NetworkInformation
 
-module Disseminate =
-    type Disseminator = private { T: obj }
+    [<Literal>]
+    let MaxSize = 512
+    
+    let connect port = 
+        let udp = new UdpClient(port = port)
+        let receivedEvent = new Event<Node * byte[]>()
 
-    // membership : Disseminator -> Node -> NodeStatus -> unit
-    let membership disseminator node status = ()
+        Async.Start(async {
+            while true do
+                let! udpMsg = udp.ReceiveAsync() |> Async.AwaitTask
+                let node = { IPAddress = udpMsg.RemoteEndPoint.Address.GetAddressBytes() |> toInt64
+                             Port = uint16 udpMsg.RemoteEndPoint.Port }
+                receivedEvent.Trigger(node, udpMsg.Buffer)
+        })
+        
+        { Client = udp
+          ReceivedEvent = receivedEvent }
+    
+    let asyncSend node bytes { Client = udp } =
+        udp.SendAsync(bytes, bytes.Length, new IPEndPoint(node.IPAddress, int node.Port))
+        |> Async.AwaitTask
+        |> Async.Ignore
 
-    // user : Disseminator -> string -> unit
-    let user disseminator event = ()
+    let received { ReceivedEvent = receivedEvent } = receivedEvent.Publish
 
 
-module Membership =
-    type MemberList =
-        private { Local : Node
-                  Incarnation : IncarnationNumber
-                  Members : Map<Node, NodeStatus>
-                  PeriodTimeout : TimeSpan }
+    let randomPort() =
+        let isUdpPortUsed =
+            let usedUdpPort =
+                IPGlobalProperties.GetIPGlobalProperties().GetActiveUdpListeners() 
+                |> Array.map (fun e -> e.Port)
 
+            fun p -> Array.exists (fun p' -> p' = p) usedUdpPort
+        
+        let rec nextFreePort port =
+            if isUdpPortUsed port then nextFreePort port
+            else port 
+
+        nextFreePort 1337
+
+
+type Disseminator = private { Events : Map<SwimEvent, uint32> }
+
+[<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Disseminator =
+    let private add event disseminator =
+        match Map.tryFind event disseminator.Events with
+        | Some _ -> disseminator
+        | None -> { disseminator with Events = Map.add event 0u disseminator.Events }
+        
+    let private maxPiggyBack numMembers =
+        if numMembers = 0 then 0
+        else
+            3. * round(log (float numMembers + 1.)) |> int
+
+    let private sortCompare (e1, cnt1) (e2, cnt2) =
+        let eventOrder = function Membership _ -> 0 | User _ -> 1
+        compare (eventOrder e1) (eventOrder e2) + (cnt1 - cnt2)
+
+    // make : () -> Disseminator
+    let make () = { Events = Map.empty }
+
+    // membership : Node -> NodeStatus -> Disseminator -> Disseminator
+    let membership node status = Membership(node, status) |> add
+
+    // user : string -> Disseminator -> Disseminator
+    let user event = User event |> add
+
+    let take numMembers maxSize disseminator =
+        let maxPiggyBack = maxPiggyBack numMembers
+
+        [], disseminator
+
+type MemberList =
+    private { Local : Node
+              Incarnation : IncarnationNumber
+              Members : Map<Node, NodeStatus>
+              PeriodTimeout : TimeSpan }
+
+[<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module MemberList =
     type private FindResult =
         | Local of IncarnationNumber
         | Member of NodeStatus
         | NotFound
 
     // disseminate : MemberList -> Node -> NodeStatus -> unit
-    let private disseminate memberList memb status =
+    let private disseminate memb status memberList =
         ()
 
     // tryFind : MemberList -> Node -> FindResult
-    let private tryFind memberList memb =
+    let private tryFind memb memberList =
         let { Local = local
               Incarnation = currentIncarnation
               Members = members } = memberList
@@ -117,7 +194,7 @@ module Membership =
             | None -> NotFound
 
     // updateMembers : MemberList -> Node -> NodeStatus -> MemberList
-    let private updateMembers memberList memb status =
+    let private updateMembers memb status memberList =
         let { Members = members } = memberList
         let members' = Map.add memb status members
         // disseminate information
@@ -126,7 +203,7 @@ module Membership =
         { memberList with Members = members' }
 
     // stillAlive : MemberList -> IncarnationNumber -> MemberList
-    let private stillAlive memberList incarnation =
+    let private stillAlive incarnation memberList =
         let { Local = local
               Incarnation = currentIncarnation } = memberList
         let incarnation' =  if currentIncarnation > incarnation then currentIncarnation
@@ -137,44 +214,44 @@ module Membership =
         { memberList with Incarnation = incarnation' }
 
     // alive : MemberList -> Node -> IncarnationNumber -> MemberList
-    let private alive memberList memb incarnation =
-        match tryFind memberList memb with
+    let private alive memb incarnation memberList =
+        match tryFind memb memberList with
         | NotFound ->
             let status = IncarnationNumber.make() |> Alive
-            updateMembers memberList memb status
+            updateMembers memb status memberList
 
         | Member(Alive i)
         | Member(Suspect i) when i < incarnation ->
-            updateMembers memberList memb (Alive incarnation)
+            updateMembers memb (Alive incarnation) memberList
 
         | Member _ -> memberList
         | Local _ -> memberList
 
     // suspect : MemberList -> Node -> IncarnationNumber -> MemberList
-    let private suspect memberList memb incarnation =
-        match tryFind memberList memb with
-        | Local i -> stillAlive memberList i
+    let private suspect memb incarnation memberList =
+        match tryFind memb memberList with
+        | Local i -> stillAlive i memberList
 
         | Member(Alive i) when incarnation >= i ->
-            updateMembers memberList memb (Suspect incarnation)
+            updateMembers memb (Suspect incarnation) memberList
 
         | Member(Suspect i) when incarnation > i ->
-            updateMembers memberList memb (Suspect incarnation)
+            updateMembers memb (Suspect incarnation) memberList
 
         | Member _ -> memberList
         | NotFound -> memberList
 
     // dead : MemberList -> Node -> IncarnationNumber -> MemberList
-    let private dead memberList memb incarnation =
-        match tryFind memberList memb with
-        | Local i -> stillAlive memberList i
+    let private dead memb incarnation memberList =
+        match tryFind memb memberList with
+        | Local i -> stillAlive i memberList
 
         | Member(Alive i)
         | Member(Suspect i) when incarnation >= i ->
-            updateMembers memberList memb (Dead i)
+            updateMembers memb (Dead i) memberList
 
         | Member(Dead i) when incarnation > i ->
-            updateMembers memberList memb (Dead i)
+            updateMembers memb (Dead i) memberList
 
         | Member _ -> memberList
         | NotFound -> memberList
@@ -187,15 +264,60 @@ module Membership =
           PeriodTimeout = periodTimeout }
 
     // update : MemberList -> Node -> Status -> MemberList
-    let update memberList node = function
-        | Alive i -> alive memberList node i
-        | Suspect i -> suspect memberList node i
-        | Dead i -> dead memberList node i
+    let update node status memberList =
+        match status with
+        | Alive i -> alive node i memberList
+        | Suspect i -> suspect node i memberList
+        | Dead i -> dead node i memberList
 
-module FailureDetection =
-    // ping : SeqNumber -> Async<bool>
-    // pingRequest : SeqNumber -> Node -> Async<bool>
-    // ack : Node -> SeqNumber -> unit
+type FailureDetector =
+    private { Timeout : TimeSpan
+              AsyncSendMessage : Node -> SwimMessage -> Async<unit>
+              AsyncReceiveMessageFor : (Node -> SwimMessage -> bool) -> TimeSpan -> Async<SwimMessage option> }
+
+[<RequireQualifiedAccess; CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module FailureDetector =
+    // ackFilter : Node -> SeqNumber -> Node -> SwimMessage -> bool
+    let private ackFilter node seqNr = fun _ -> 
+        function Ack(s, n) -> seqNr = s && node = n | _ -> false
+
+    // sendMessage : FailureDetector -> Node -> SwimMessage
+    let private sendMessage detector node message = async {
+        do! detector.AsyncSendMessage node message
+        return None
+    }
+
+    // make : TimeSpan -> (Node -> SwimMessage -> Async<unit>) -> ((Node -> SwimMessage -> bool) -> TimeSpan -> Async<SwimMessage option>) -> FailureDetector
+    let make timeout asyncSendMessage asyncReceiveMessageFor =
+        { Timeout = timeout
+          AsyncSendMessage = asyncSendMessage
+          AsyncReceiveMessageFor = asyncReceiveMessageFor }
+
+    // ping : SeqNumber -> Node -> FailureDetector -> Async<bool>
+    let ping seqNr node detector = async {
+        let! results =
+            [ detector.AsyncReceiveMessageFor (ackFilter node seqNr) detector.Timeout
+              Ping seqNr |> sendMessage detector node ]
+            |> Async.Parallel
+
+        return Array.head results |> Option.isSome
+    }
+
+    // pingRequest : SeqNumber -> Node -> Node list -> FailureDetector -> Async<bool>
+    let pingRequest seqNr node nodes detector = async {
+        let messages = nodes |> List.map (fun n -> PingRequest(seqNr, node) |> sendMessage detector n)
+
+        let! results =
+            detector.AsyncReceiveMessageFor (ackFilter node seqNr) detector.Timeout :: messages
+            |> Async.Parallel
+
+        return Array.head results |> Option.isSome
+    }
+
+    // ack : SeqNumber -> Node -> Node -> FailureDetector -> unit
+    let ack seqNr node replyNode detector = 
+        Ack(seqNr, node) |> detector.AsyncSendMessage replyNode
+
 
 module Swim =
     type Config =
@@ -204,3 +326,16 @@ module Swim =
           PingTimeout : TimeSpan
           PingRequestGroupSize : int
           SuspectTimeout : TimeSpan }
+
+    // make : unit -> IDisposable
+    
+    // handle : Node -> SwimMessage -> detector
+    (*let handle source message detector =
+        match message with
+        | Ping seqNr ->
+            ack seqNr detector.Local source detector
+        | PingRequest(seqNr, node) -> async {
+            let! acked = ping seqNr node detector
+            if acked then
+                do! ack seqNr node source detector }
+        | Ack _ -> async { () }*)
